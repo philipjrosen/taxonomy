47a121d069b655b52c7e5dd32460c6892a67f8a0
Philip Rosen <philipjrosen@gmail.com>
2014-12-15 21:17:05 -0800

    adds all bees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "Keep on growing";
 };
-
+//want Bee.prototype to delegate to Grub.prototype
+Bee.prototype = Object.create(Grub.prototype);
+//add back the overwritten constructor property
+//and make sure it points to the Bee constructor
+Bee.prototype.constructor = Bee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
+};
+//make thie HMB prototype delegate to Bee.prototype
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+//add this overwritten constructor
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
 };
 
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
